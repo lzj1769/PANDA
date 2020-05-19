ddaae242b3ea47594d790ab0924e41a972835c43
lzj1769 <lzj1769@gmail.com>
2020-05-19 21:35:47 +0200

    add

--- src/call_cluster_gpu.py
+++ src/call_cluster_gpu.py
@@ -1,7 +1,7 @@
 import subprocess
 
 model_list = ['se_resnext50_32x4d']
-fold_list = [1, 2, 3, 4]
+fold_list = [2]
 
 batch_size = {'se_resnext50_32x4d': 24,
               'se_resnext101_32x4d': 4}
