981023c9088cd446278d6ec8faa0f342b357c6b4
lzj1769 <lzj1769@gmail.com>
2020-05-26 22:28:24 +0200

    add

--- src/call_cluster_gpu.py
+++ src/call_cluster_gpu.py
@@ -3,14 +3,14 @@ import subprocess
 model_list = ['se_resnext50_32x4d']
 fold_list = [0]
 
-batch_size = {'se_resnext50_32x4d': 5,
+batch_size = {'se_resnext50_32x4d': 2,
               'se_resnext101_32x4d': 6,
               'inceptionv4': 6,
               'inceptionresnetv2': 8}
 
 level = 1
 patch_size = 256
-num_patches = 16
+num_patches = 32
 
 for model in model_list:
     for fold in fold_list:
@@ -18,7 +18,7 @@ for model in model_list:
         subprocess.run(["sbatch", "-J", job_name,
                         "-o", f"./cluster_out/{job_name}.txt",
                         "-e", f"./cluster_err/{job_name}.txt",
-                        "--time", "30:00:00",
+                        "--time", "60:00:00",
                         "--mem", "180G",
                         "-c", "48",
                         "-A", "rwth0233",
